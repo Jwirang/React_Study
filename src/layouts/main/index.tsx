781cbd92ab08322b46964638f1e2bc00b7ca1230
Jwirang <haha71119@gamil.com>
2024-06-09 00:47:12 +0900

    feat: 로그아웃 기능 추가

--- src/layouts/main/index.tsx
+++ src/layouts/main/index.tsx
@@ -3,6 +3,8 @@ import { Link, Outlet, useNavigate } from "react-router-dom";
 import { fetchMe } from "../../api/fatBrainApi";
 import fatbrain from '../../assets/fatbrainImage.png';
 import { LogoContainer, LogoImage, StyledH1, StyledHeaderContainer, StyledLink } from "./style";
+import Cookies from "js-cookie";
+import fatBrainClient from "../../api/fatBrainApi/client";
 
 const Main = () => {
   const navigate = useNavigate();
@@ -10,8 +12,13 @@ const Main = () => {
     queryKey: ['me'],
     queryFn: fetchMe,
   })
-
   if (isError) {
+    navigate('/login')
+  }
+
+  const logOut = () => {
+    Cookies.remove('tid')
+    delete fatBrainClient.defaults.headers['Authorization']
     navigate('/login')
   }
 
@@ -20,15 +27,14 @@ const Main = () => {
       <header>
         <StyledHeaderContainer>
         <LogoContainer>
-          <Link to='/login'>
-            <LogoImage src={fatbrain} alt="사이트 로고" /> {/* 로고 이미지 추가 */}
-          </Link>
+          <LogoImage src={fatbrain} alt="사이트 로고" /> {/* 로고 이미지 추가 */}
         </LogoContainer>
           <StyledH1>
             <StyledLink to="/">
               {me?.nickname ?? me?.username}님의 지식 냠냠 목록🍚
             </StyledLink>
           </StyledH1>
+          <button onClick={logOut}>로그아웃</button>
         </StyledHeaderContainer>
       </header>
       <Outlet />
